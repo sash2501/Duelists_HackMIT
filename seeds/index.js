85e40aea9f957e9f309788ab591bd53787a8aecb
Sashmita Raghav <sashmitaraghav2505@gmail.com>
2021-09-19 23:42:17 +0530

    Update files

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Incident.deleteMany({});
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 5; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     // const price = Math.floor(Math.random() * 20) + 10;
     const time = "00:00";
